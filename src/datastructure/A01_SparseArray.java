69127e6141cea9032f3f0878519852031e5186ee
xiahuya <xia__hu@163.com>
2023-02-28 20:30:34 +0800

    数据结构-稀疏数组

--- src/datastructure/A01_SparseArray.java
+++ src/datastructure/A01_SparseArray.java
@@ -17,7 +17,6 @@ public class A01_SparseArray {
         // 元素 1 表示黑棋
         // 元素 2 表示白旗
 
-
         array[1][2] = 1;
         array[2][3] = 2;
         array[3][4] = 2;
@@ -40,7 +39,7 @@ public class A01_SparseArray {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 int data = array[i][j];
-                if (data > 0) {
+                if (data != 0) {
                     sum++;
                 }
             }
@@ -59,15 +58,13 @@ public class A01_SparseArray {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 int data = array[i][j];
-                if (data > 0) {
+                if (data != 0) {
                     sparseArray[x][0] = i;
                     sparseArray[x][1] = j;
                     sparseArray[x][2] = data;
                     x ++;
                 }
-
             }
-
         }
 
         //4、打印稀疏数组
@@ -78,6 +75,30 @@ public class A01_SparseArray {
             }
             System.out.println();
         }
+        System.out.println("******************************************");
 
+        //B、稀疏数组 --> 二维数组
+        //1、读取稀疏数组第一行数据,创建二维数组
+        int m =  sparseArray[0][0];
+        int n =  sparseArray[0][1];
+
+        int[][] array2 = new int[m][n];
+
+        //2、遍历稀疏数组剩余的元素
+        for (int i = 1; i < sparseArray.length; i++) {
+            int x1 = sparseArray[i][0];
+            int y1 = sparseArray[i][1];
+            int data1 = sparseArray[i][2];
+            array2[x1][y1] = data1;
+        }
+
+        //3、打印二维数组
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                int data = array2[i][j];
+                System.out.print(String.format("%d\t", data));
+            }
+            System.out.println();
+        }
     }
 }
